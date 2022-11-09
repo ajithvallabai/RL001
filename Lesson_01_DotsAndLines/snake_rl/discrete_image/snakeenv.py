4988e302299882529f1d9af7519091ddfd12de72
ajithvallabai <inocajith21.5@gmail.com>
2022-11-09 14:53:53 +0530

    Add train,inference, border to enve

--- Lesson_01_DotsAndLines/snake_rl/discrete_image/snakeenv.py
+++ Lesson_01_DotsAndLines/snake_rl/discrete_image/snakeenv.py
@@ -7,14 +7,26 @@ import copy
 
 FOOD_FETCH_GOAL = 15
 
-def snakeBody(dot, img):
+def snakeBody(dot, foodLoc):
+    img = np.zeros((632, 632, 3), dtype="uint8")
+    # Add border
+    boundaryPts = np.array([[30, 30], [30, 542],
+                    [542, 542], [542, 30]],
+                np.int32)
+    boundaryPts = boundaryPts.reshape((-1, 1, 2))
+    isClosed = True
+    thickness = 8
+    img = cv.polylines(img, [boundaryPts],
+                        isClosed, (0, 0, 255),
+                        thickness)
+    cv.rectangle(img, foodLoc, foodLoc, (0,255,0), 10)
     cv.rectangle(img, dot, dot, (255,255,255), 5)
     return img
 
 def dtCollisionBoundaries(dot):
-    if (dot[0] >= 512) or (dot[0] <= 0):
+    if (dot[0] >= 542) or (dot[0] <= 30):
         return True
-    if (dot[1] >= 512) or (dot[1] <= 0):
+    if (dot[1] >= 542) or (dot[1] <= 30):
         return True
     return False
 
@@ -23,9 +35,9 @@ def dtFood(dot, arrFood):
         return True
     return False
 
-BUFFER_MEMORY = 4
-XDIM = 64
-YDIM = 64
+BUFFER_MEMORY = 50
+XDIM = 40
+YDIM = 40
 class SnakeEnv(Env):
     """Snake Environment that follows gym interface"""
 
@@ -38,7 +50,7 @@ class SnakeEnv(Env):
 
     def step(self, action):
         #self.prev_actions.append(action)
-        cv.imshow("The Slytherin Dot game", self.img)
+        #cv.imshow("The Slytherin Dot game", self.img)
         self.key = action
         previousArr = copy.deepcopy(self.dot)
 
@@ -60,16 +72,13 @@ class SnakeEnv(Env):
         # check if snake has found the food
         currFoodFetch = False
         if dtFood(self.dot, self.foodLoc) == True:
-            self.foodLoc = [random.randint(1, 511), random.randint(1, 511)]
+            self.foodLoc = [random.randint(31, 540), random.randint(31, 540)]
             self.score += 1
             currFoodFetch = True
             print("Food fetched")
 
-        self.img = np.zeros((512, 512, 3), dtype="uint8")
-        cv.rectangle(self.img, self.foodLoc, self.foodLoc, (0,255,0), 10)
-
-        self.img = snakeBody(self.dot, self.img)
-        cv.waitKey(150)
+        self.img = snakeBody(self.dot, self.foodLoc )
+        #cv.waitKey(150)
 
         # Using distance parameters and awarding reward
         currDistToFood = np.linalg.norm(np.array(self.dot)-np.array(self.foodLoc))
@@ -77,24 +86,24 @@ class SnakeEnv(Env):
 
         if self.done:
             # For colliding with boundaries
-            self.reward -= 20
+            self.reward -= 200
         elif currFoodFetch:
             # Fetching current food
-            self.reward += (self.score * 100)
+            self.reward += (self.score * 1000)
         elif currDistToFood < prevDistToFood:
             # staying alive and moving towards from food
-            self.reward += 1
+            self.reward += 3
         else:
             # staying alive and moving away from food
             self.reward -= 1
 
-        self.previousFrames = self.previousFrames[1:] #poping first frame
+        self.previousFrames = self.previousFrames[:, :, 3:] #poping first frame
 
-        currFrame = self.img[(self.dot[1]-32): (self.dot[1]+32), (self.dot[0]-32): (self.dot[0] + 32)]
+        currFrame = self.img[(self.dot[1]-(YDIM//2)): (self.dot[1]+(YDIM//2)), (self.dot[0]-(XDIM//2)): (self.dot[0] + (XDIM//2))]
         # cv.imshow("The frame ", currFrame)
         # cv.waitKey(0)
-        self.previousFrames.append(currFrame)
-        self.observation = np.swapaxes(self.previousFrames,2,1).reshape(XDIM,YDIM,3*BUFFER_MEMORY)
+        self.previousFrames = np.dstack((self.previousFrames, currFrame))
+        self.observation = copy.deepcopy(self.previousFrames)
         # cv.imshow("The frame ", self.observation[:,:, :3])
         # cv.waitKey(0)
         info = {}
@@ -102,21 +111,23 @@ class SnakeEnv(Env):
 
     def reset(self):
         self.done = False
-        self.img = np.zeros((512, 512, 3), dtype="uint8")
+        #self.img = np.zeros((512, 512, 3), dtype="uint8")
 
         # Food
-        self.foodLoc = [random.randint(1, 511), random.randint(1, 511)]
-        cv.rectangle(self.img, self.foodLoc, self.foodLoc, (0,255,0), 10)
+        self.foodLoc = [random.randint(31, 540), random.randint(31, 540)]
+        #cv.rectangle(self.img, self.foodLoc, self.foodLoc, (0,255,0), 10)
         self.score = 0
         self.reward = 0
         self.dot = [410,320]
-        self.img = snakeBody(self.dot, self.img)
+        self.img = snakeBody(self.dot, self.foodLoc)
         self.key = 0 # default key left
 
         # observation
-        self.previousFrames = []
-        for _ in range(BUFFER_MEMORY):
-            self.previousFrames.append(np.zeros((XDIM, YDIM, 3),dtype=np.uint8))
+        self.previousFrames = np.zeros((XDIM, YDIM, 3),dtype=np.uint8)
+        b = np.zeros((XDIM, YDIM, 3),dtype=np.uint8)
 
-        self.observation = np.swapaxes(self.previousFrames,2,1).reshape(XDIM,YDIM,3*BUFFER_MEMORY)
+        for _ in range(BUFFER_MEMORY-1):
+            self.previousFrames = np.dstack((self.previousFrames, b))
+
+        self.observation = copy.deepcopy(self.previousFrames)
         return self.observation
