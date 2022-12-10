529f184f498c5dd1f9fc59270da024dff243306f
ajithvallabai <inocajith21.5@gmail.com>
2022-12-10 11:44:47 +0530

    Replace arr with dot

--- Lesson_01_DotsAndLines/basic_games/snake.py
+++ Lesson_01_DotsAndLines/basic_games/snake.py
@@ -3,19 +3,19 @@ import numpy as np
 import random
 import copy
 
-def snakeBody(arr, img):
-    cv.rectangle(img, arr, arr, (255,255,255), 5)
+def snakeBody(dot, img):
+    cv.rectangle(img, dot, dot, (255,255,255), 5)
     return img
 
-def dtCollisionBoundaries(arr):
-    if (arr[0] >= 512) or (arr[0] <= 0):
+def dtCollisionBoundaries(dot):
+    if (dot[0] >= 512) or (dot[0] <= 0):
         return True
-    if (arr[1] >= 512) or (arr[1] <= 0):
+    if (dot[1] >= 512) or (dot[1] <= 0):
         return True
     return False
 
-def dtFood(arr, arrFood):
-    if np.linalg.norm(np.array(arr)-np.array(foodArr)) < 14:
+def dtFood(dot, foodDot):
+    if np.linalg.norm(np.array(dot)-np.array(foodDot)) < 14:
         return True
     return False
 
@@ -23,41 +23,41 @@ if __name__ == "__main__":
     img = np.zeros((512, 512, 3), dtype="uint8")
 
     # Food
-    foodArr = [random.randint(1, 511), random.randint(1, 511)]
-    cv.rectangle(img, foodArr, foodArr, (0,255,0), 10)
+    foodDot = [random.randint(1, 511), random.randint(1, 511)]
+    cv.rectangle(img, foodDot, foodDot, (0,255,0), 10)
 
-    arr = [410,320]
-    img = snakeBody(arr, img)
+    dot = [410,320]
+    img = snakeBody(dot, img)
     key = 97 # default key left
 
     while True:
         done = False
         cv.imshow("The Slytherin Dot game", img)
         if key == ord('a'):
-            arr[0] = arr[0] - 5
+            dot[0] = dot[0] - 5
         elif key == ord('d'):
-            arr[0] = arr[0] + 5
+            dot[0] = dot[0] + 5
         elif key == ord('w'):
-            arr[1] = arr[1] - 5
+            dot[1] = dot[1] - 5
         elif key == ord('s'):
-            arr[1] = arr[1] + 5
+            dot[1] = dot[1] + 5
         elif key == ord('q'):
             break
 
         # detect collision with boudaries
-        if dtCollisionBoundaries(arr) == True:
+        if dtCollisionBoundaries(dot) == True:
             print("collision with boundaries")
             break
 
         # check if snake has found the food
-        if dtFood(arr, foodArr) == True:
-            foodArr = [random.randint(1, 511), random.randint(1, 511)]
+        if dtFood(dot, foodDot) == True:
+            foodDot = [random.randint(1, 511), random.randint(1, 511)]
             print("Food fetched")
 
         img = np.zeros((512, 512, 3), dtype="uint8")
-        cv.rectangle(img, foodArr, foodArr, (0,255,0), 10)
+        cv.rectangle(img, foodDot, foodDot, (0,255,0), 10)
 
-        img = snakeBody(arr, img)
+        img = snakeBody(dot, img)
         k = cv.waitKey(400)
         if k != -1:
             key = copy.deepcopy(k)
